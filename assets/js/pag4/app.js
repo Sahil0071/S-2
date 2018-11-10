81681a2bbf64e674d3a2d9a7e3fac561dd1920fc
Sahil0071 <sahilraja242@gmail.com>
2018-11-10 12:00:09 +0530

    First commit

---
+++ assets/js/pag4/app.js
@@ -0,0 +1,53 @@
+function populate() {
+    if (quiz.isEnded()) {
+        showScores();
+    } else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for (var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your score: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions
+var questions = [
+    new Question("What is that one thing that is required in order to cook something?", ["water", "salt", "chulha/gas stove", "sugar"], "chulha/gas stove"),
+
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
---
+++ assets/js/pag5/app.js
@@ -0,0 +1,53 @@
+function populate() {
+    if (quiz.isEnded()) {
+        showScores();
+    } else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for (var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your score: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions
+var questions = [
+    new Question("What will you do if 2 of your close childhood friends arrive to give you a surprise just before you added the rice?They seem to have a hectic journey they really tired and hungry.", ["Only give them a glass of water", "Tell them to go away", "Ask them if they have brought any gifts for you", "Change the quantity of ingredients and prepare lunch for them also"], "Change the quantity of ingredients and prepare lunch for them also"),
+
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
